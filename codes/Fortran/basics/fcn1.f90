927040491c153a66f6ae968b7131229697f561a1
Aniket K <t23189@students.iitmandi.ac.in>
2024-03-15 18:23:57 +0530

    changes done

--- codes/Fortran/basics/fcn1.f90
+++ codes/Fortran/basics/fcn1.f90
@@ -3,16 +3,16 @@
 program fcn1
     implicit none
     real(kind=8) :: y,z
-    real(kind=8), external :: f
+    real(kind=8), external :: fun
 
     y = 2.
-    z = f(y)
+    z = fun(y)
     print *, "z = ",z
 end program fcn1
 
-real(kind=8) function f(x)
+real(kind=8) function fun(x)
     implicit none
     real(kind=8), intent(in) :: x
-    f = x**2
-end function f
+    fun = x**2
+end function fun
 
