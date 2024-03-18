f758b5e88e79b46d446da16e60573b8664c1247e
Aniket K <t23189@students.iitmandi.ac.in>
2024-03-18 19:19:37 +0530

    test_case also included

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,7 +1,34 @@
-x = 3.0
-s = 1.0
-kmax = 10
-for k in range(kmax):
-	s = 0.5 * (s + x/s)
+"""
+my module of sqrt
 
-print(s)
+"""
+def sqrt2(x, debug = False):
+	"""
+	msqrt.sqrt2
+	"""
+	from numpy import nan
+	if x==0:
+		return 0
+	elif x<0:
+		return nan
+	s = 1.0
+	kmax = 10
+	tol = 0.001
+	for k in range(kmax):
+		if debug:
+			print(f"At iteration {k} the value of s = {s}")
+		s0 = s
+		s = 0.5 * (s + x/s)
+		delta_s = s - s0
+		if(abs(delta_s)/x < tol):
+			break
+		if debug:
+			print(f"actual s = {s}")
+			print(f"Finally, the value of s = {s:1.4f}")
+	return s
+
+if __name__ == "__main__":
+	from test_case import test_1
+	print("in main")
+	test_1()
+	
